31f4fa8d7cf0eb229fe58231cbd459c564b39573
shubham <shubhamganeshsonawane11@gmail.com>
2025-01-24 21:57:56 +0530

    Add

---
+++ 22Day_Binary_Search_Trees (BST)/1Search_in_a_Binary_Search_Tree copy.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+
+class Solution{
+    public:
+    Node* SearchBST(TreeNode* root, int val){
+        //base case
+        if(root == NULL ) return NULL;
+        if(root->val == val){
+            return root;
+        }else if(root->left > val){
+            return SearchBST(root->left, val);
+        }else{
+            return SearchBST(root->right, val);
+        }
+    }
+};
