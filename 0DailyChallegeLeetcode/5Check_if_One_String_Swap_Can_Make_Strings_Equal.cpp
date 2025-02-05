f9467e18d20d0934a692e32815300c29ebc86343
shubham <shubhamganeshsonawane11@gmail.com>
2025-02-05 12:44:28 +0530

    Add

---
+++ 0DailyChallegeLeetcode/5Check_if_One_String_Swap_Can_Make_Strings_Equal.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        if(s1 == s2) return true;
+        string a = s1;
+        string b = s2;
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        if(a!=b) return false;
+        else{
+            vector<int>v;
+            for(int i=0;i<s1.size();i++)
+                if(s2[i]!=s1[i]) 
+                v.push_back(i);
+                if(v.size()!=2) return false;
+                else{
+                    swap(s2[v[0]],s2[v[1]]);
+                    if(s1==s2) return true;
+                    else return false;
+                
+            }
+        }
+    }
+};
+
+int main(){
+    string s1 = "bank";
+    string s2 = "kanb";
+    Solution obj;
+    int ans = obj.areAlmostEqual(s1,s2);
+    if(ans == 1){
+        cout<<"True";
+    }
+    else{
+        cout<<"false";
+    }
+}
