8e7bb1e1c18819652c4d568263e312c2708460fd
shubham <shubhamganeshsonawane11@gmail.com>
2025-02-07 11:25:31 +0530

    Add

---
+++ 0DailyChallegeLeetcode/37Find _Number_of_Distinct_Colors_Among_Balls.cpp
@@ -0,0 +1,29 @@
+// 3160. Find the Number of Distinct Colors Among the Balls
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        map<int, set<int>> mpp;
+        map<int, int> mp;
+        vector<int> ans;
+        for(auto it: queries){
+            int color = it[1];
+            int ball = it[0];
+
+            if(mp.find(ball) == mp.end()){
+                mp[ball] = color;
+                mpp[color].insert(ball);
+            }
+            else{
+                int c = mp[ball];
+                mpp[c].erase(ball);
+                if(mpp[c].empty()) {
+                    mpp.erase(c);
+                }
+                mp[ball] = color;
+                mpp[color].insert(ball);
+            }
+            ans.push_back(mpp.size());
+        }
+        return ans;
+    }
+};
